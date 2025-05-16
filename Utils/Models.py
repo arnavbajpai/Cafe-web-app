0b0c0fe51547d6e122f16cd3c5ee5b60066b905c
Arnav Bajpai <arnavbcool@gmail.com>
2025-05-16 21:56:36 +0400

    Implement data validation constraints

---
+++ Utils/Models.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, Field, EmailStr
+from enum import Enum 
+from uuid import UUID
+class Gender(str, Enum):
+    male = 'Male'
+    female = 'Female'
+
+class Employee(BaseModel):
+    id: str = Field(pattern=r'^UI\d{7}$')
+    name: str
+    email: EmailStr
+    phone: str = Field(pattern=r'^[89]\d{7}$')
+    gender: Gender
+
+class Cafe(BaseModel):
+    id: UUID
+    name: str
+    description: str
+    location: str
+    logo: str | None
+
+class UpdateCafe(BaseModel):
+    name: str | None = Field(min_length=6, max_length=10)
+    description: str | None = Field(max_length=255)
+    location: str | None
+    logo: str | None = Field(max_size=2*1024*1024)
+
+class UpdateEmployee(BaseModel):
+    name: str | None = Field(min_length=6, max_length=10)
+    email: EmailStr | None 
+    phone: str | None = Field(length=8, pattern=r'^[89]\d{7}$')
+    gender: Gender | None
+    cafe: str | None
