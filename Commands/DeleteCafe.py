05ed1e2d255525187507c744cfa117928b1720a3
Arnav Bajpai <arnavbcool@gmail.com>
2025-05-21 15:11:43 +0400

    Fixed cafe delete logic

--- Commands/DeleteCafe.py
+++ Commands/DeleteCafe.py
@@ -1,8 +1,20 @@
 from Database.Database import get_session
-from Models.DatabaseModels import CafeDB
+from Models.DatabaseModels import CafeDB, EmployeeCafeDB
+from Commands.DeleteEmployee import delete_employee
 
 from Utils.Messages import CAFE_DELETED
 from Utils.Exceptions import server_error_exception, cafe_not_found_exception
+
+
+def get_employee_ids_by_cafe(session, cafe_id):
+    emp_cafe_records = session.query(EmployeeCafeDB).filter(EmployeeCafeDB.cafeId == cafe_id).all()
+    return [record.empId for record in emp_cafe_records]
+
+
+def delete_employees_by_ids(session, emp_ids):
+    for emp_id in emp_ids:
+        delete_employee(emp_id)
+
 
 def delete_cafe(cafe_id: str):
     with get_session() as session:
@@ -11,9 +23,11 @@ def delete_cafe(cafe_id: str):
             cafe_not_found_exception(cafe_id)
         name = cafe.cafeName
         try:
+            emp_ids = get_employee_ids_by_cafe(session, cafe_id)
+            delete_employees_by_ids(session, emp_ids)
             session.delete(cafe)
             session.commit()
-            return {"message": CAFE_DELETED.format(cafe_name = name, cafe_id = cafe_id)}
+            return {"message": CAFE_DELETED.format(cafe_name=name, cafe_id=cafe_id)}
         except Exception as e:
             session.rollback()
             server_error_exception(e)
